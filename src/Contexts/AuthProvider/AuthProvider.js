3d11d3e6362d22d0817d4e656cd49104e68c99e2
Sifat-Khan-cr <pskcrjr@gmail.com>
2022-10-27 02:12:52 +0600

    google popup signIn done

--- src/Contexts/AuthProvider/AuthProvider.js
+++ src/Contexts/AuthProvider/AuthProvider.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import { createContext } from 'react';
+import app from '../../Firebase/firebase.init';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
 export const AuthContext = createContext();
 
 
+
 const AuthProvider = ({ children }) => {
+    const googleAuthProvider = new GoogleAuthProvider();
+    const auth = getAuth(app);
+
+
+    const googleHandler = () => {
+        signInWithPopup(auth, googleAuthProvider)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+            })
+            .catch(error => console.error(error))
+    }
+
+
     const user = { name: 'sifat' };
 
-    const authInfo = { user };
+    const authInfo = { user, googleHandler };
     return (
         <AuthContext.Provider value={authInfo}>
             {children}
--- src/Pages/Shared/Login/Login.js
+++ src/Pages/Shared/Login/Login.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { useContext } from 'react';
 import Button from 'react-bootstrap/Button';
+import { AuthContext } from '../../../Contexts/AuthProvider/AuthProvider';
 
 const Login = () => {
+    const { googleHandler } = useContext(AuthContext);
     return (
         <div>
             <h1>this is login page</h1>
-            <Button variant="success">Google</Button>
+            <Button onClick={googleHandler} variant="success">Google</Button>
             <Button variant="success">Twitter</Button>
 
         </div>
